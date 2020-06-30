b286eb27ce487822ac060778019f74b84f41d3a6
tyysun <tyysun@outlook.com>
2020-06-30 23:32:57 +0800

    :white_check_mark: 增加单元测试

---
+++ __tests__/db.spec.js
@@ -0,0 +1,25 @@
+const db = require("../db.js")
+const fs = require("fs")
+jest.mock("fs")
+
+describe("db", () => {
+  afterEach(() => {
+    fs.clearMocks()
+  })
+  it("can read", async () => {
+    const data = [{ title: "i am tyy", done: true }]
+    fs.setReadFileMock("/xxx", null, JSON.stringify(data))
+    const list = await db.read("/xxx")
+    expect(list).toStrictEqual(data)
+  })
+  it("can write", async () => {
+    let fakeFile
+    fs.setWriteFileMock("/yyy", (path, data, callback) => {
+      fakeFile = data
+      callback(null)
+    })
+    const list = [{ title: "style jest", done: true }]
+    await db.write(list, "/yyy")
+    expect(fakeFile).toBe(JSON.stringify(list) + "\n")
+  })
+})
